a5f455d097c8216e2f27859318d340d106551093
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-02 15:08:50 -0500

    changes some files, basic comments and framework put in place for readme generator

---
+++ index.js
@@ -0,0 +1,12 @@
+// use NPM - initialize and run to include package.json and inquirer. 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// prompts for information for README file. 
+inquirer. prompt([
+
+])
+// put prompts into readme file for creation. 
+.then((data) => {
+
+});
