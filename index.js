a12b47f56a3e2c759f2f6f3ab619a87b87f5ba57
Dexter Griffith <dextergriffith@sbcglobal.net>
2021-10-02 20:22:57 -0500

    added questions to prompt user for input

--- index.js
+++ index.js
@@ -2,11 +2,60 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
 // prompts for information for README file. 
 inquirer. prompt([
-
+    {
+        //readme generator explanation of app 
+        message: "What is the Title of your Project?",
+        name: "title",
+        type: "input",
+    },
+    {
+        message: "Write a short description of your project",
+        name: "description",
+        type: "input",
+    },
+    {
+        message: "List out the steps needed to complete this project",
+        name: "tasks completed",
+        type: "input",
+    },
+    {
+        message: "How can the user install your app?",
+        type: "input",
+        name: "installation",
+    },
+    {
+        message: "Who gets the credit for this project?",
+        type: "input",
+        name: "credits",
+    },
+    {
+        message: "what resources helped you create this project",
+        type: "input",
+        name: "references",
+    },
+    {
+        message: "what license would you like to use?",
+        type: "list",
+        name: "license",
+        choices: [
+            "MIT license",
+            "BSD 2-clause license", 
+            "N/A"
+        ]
+    }
 ])
 // put prompts into readme file for creation. 
 .then((data) => {
-
+// get it to write onto a readme file. 
+    fs.writeFile('README.md', htmlContent,(err) => {
+        if (err) {
+            console.log('ERROR',err);
+        }
+        else {
+            console.log('SUCCESS you created a README.md file!');
+        }
+    });
 });
